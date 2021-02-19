c793bca8ffde6d77ec14b5fd3236855b91a4ec91
Andrew Mason <andrewmcodes@protonmail.com>
2021-02-19 17:40:50 -0500

    chore: initial commit

---
+++ lib/release/please/demo.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "demo/version"
+
+module Release
+  module Please
+    module Demo
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
